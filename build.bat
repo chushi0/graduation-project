cb47fe8b90f33bcbbfbaedf344f94feca44271b3
初始状态0 <chushi0@users.noreply.github.com>
2022-01-02 17:46:51 +0800

    qscintilla lib

--- build.bat
+++ build.bat
@@ -11,14 +11,16 @@ set WORKSPACE=%cd%
 del /F /S /Q "%INSTALL_PATH%" > NUL 2> NUL
 mkdir %INSTALL_PATH% > NUL 2> NUL
 
-cd cpp
-cd main
+cd %WORKSPACE%\cpp\main
+cmd /C build.bat
+cd %WORKSPACE%\cpp\qscintilla
 cmd /C build.bat
 
 cd %WORKSPACE%\golang\startup
 cmd /C build.bat
 
 cd %INSTALL_PATH%
+for %%i in (*.dll) do "%QT_BIN%\windeployqt" "%%i"
 for %%i in (*.exe) do "%QT_BIN%\windeployqt" "%%i"
 
 cd %WORKSPACE%
---
+++ cpp/qscintilla/build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+copy *.dll %INSTALL_PATH% > NUL 2> NUL
